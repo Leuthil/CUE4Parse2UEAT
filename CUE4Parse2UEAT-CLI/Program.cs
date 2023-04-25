e75b2285ebfeba41f3c6bc6aee7cbd7156647792
Leuthil <leuthil@gmail.com>
2023-04-24 22:31:44 -0400

    Enhance example CLI project to be usable by others

--- CUE4Parse2UEAT-CLI/Program.cs
+++ CUE4Parse2UEAT-CLI/Program.cs
@@ -6,20 +6,109 @@ namespace CUE4Parse2UEAT_CLI
     {
         static void Main(string[] args)
         {
-            string gameDirectory = @"C:\Games\Steam\SteamApps\common\Hogwarts Legacy\Phoenix";
-            string exportDirectory = @"F:\Repos\HogwartsModding\TestDump";
+            try
+            {
+                if (!ValidateArgs(args, out string gameDirectory, out string exportDirectory, out string assetPackagePath))
+                {
+                    WriteHelp();
+                    return;
+                }
 
-            // CUE4ParseGameFileProvider is hard-coded for Hogwarts Legacy
-            var provider = new CUE4ParseGameFileProvider(gameDirectory);
-            var exporter = new CUE4ParseGameFileExporter(provider);
-            exporter.Initialize(new ExporterConfig.Builder().SetExportDirectory(exportDirectory).Build());
+                // CUE4ParseGameFileProvider is hard-coded for Hogwarts Legacy
+                var provider = new CUE4ParseGameFileProvider(gameDirectory);
+                var exporter = new CUE4ParseGameFileExporter(provider);
+                exporter.Initialize(new ExporterConfig.Builder().SetExportDirectory(exportDirectory).Build());
+                
+                var gameFile = provider.GetGameFile(assetPackagePath);
+                exporter.Export(gameFile);
 
-            //var assetFullPath = @"Phoenix/Content/UI/Conjuration/UI_T_ChalkboardSign_Conjuration.uasset";
-            //var assetFullPath = @"Phoenix/Content/UI/Conjuration/UI_BP_ConjurationScreen.uasset";
-            var assetFullPath = @"Phoenix/Content/Gameplay/BluePrints/Universal/BP_WaveSpawner.uasset";
+                Console.WriteLine($"Completed export of \"{gameFile?.Path}\"");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unexpected exception: " + ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
 
-            var gameFile = provider.GetGameFile(assetFullPath);
-            exporter.Export(gameFile);
+            Console.Out.WriteLine();
+            Console.Out.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        static bool ValidateArgs(string[] args, out string gameDirectory, out string exportDirectory, out string assetPackagePath)
+        {
+            gameDirectory = string.Empty;
+            exportDirectory = string.Empty;
+            assetPackagePath = string.Empty;
+
+            if (args.Length < 3)
+            {
+                Console.Out.WriteLine($"Error: Expecting 3 args; only received {args.Length}");
+                Console.Out.WriteLine();
+                return false;
+            }
+
+            if (args.Length > 3)
+            {
+                Console.Out.WriteLine($"Error: Expecting 3 args but received {args.Length}; ignore the extras...");
+                Console.Out.WriteLine();
+            }
+
+            try
+            {
+                gameDirectory = Path.GetFullPath(args[0]);
+
+                if (!Directory.Exists(gameDirectory))
+                {
+                    Console.Out.WriteLine($"Error: Game directory {gameDirectory} does not exist");
+                    Console.Out.WriteLine();
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Out.WriteLine($"Error: Game directory path is not valid: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+                Console.Out.WriteLine();
+                return false;
+            }
+
+            try
+            {
+                exportDirectory = Path.GetFullPath(args[1]);
+
+                if (!Directory.Exists(exportDirectory))
+                {
+                    Console.Out.WriteLine($"Export directory {exportDirectory} does not exist; creating it...");
+                    Console.Out.WriteLine();
+                    Directory.CreateDirectory(exportDirectory);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Out.WriteLine($"Error: Export directory path is not valid: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+                Console.Out.WriteLine();
+                return false;
+            }
+
+            assetPackagePath = args[2];
+
+            return true;
+        }
+
+        static void WriteHelp()
+        {
+            Console.Out.WriteLine("Usage:");
+            Console.Out.WriteLine("\tCUE4Parse2UEAT-CLI.exe gameDir exportDir assetPackagePath");
+            Console.Out.WriteLine();
+            Console.Out.WriteLine("Example:");
+            Console.Out.WriteLine("\tCUE4Parse2UEAT-CLI.exe \"C:\\Steam\\SteamApps\\common\\Hogwarts Legacy\\Phoenix\" \"C:\\Dump\" \"Phoenix/Content/Pawn/Player/BP_Biped_Player.uasset\"");
+            Console.Out.WriteLine();
+            Console.Out.WriteLine("\tgameDir\t\t\tGame content directory.");
+            Console.Out.WriteLine("\texportDir\t\tDirectory where asset json is exported to.");
+            Console.Out.WriteLine("\tassetPackagePath\tPackage path of asset to export.");
+            Console.Out.WriteLine();
         }
     }
 }
